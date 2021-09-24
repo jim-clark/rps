42e5ade2ebe70a06be3372094e149ac269117029
Jim Clark <jim.clark@generalassemb.ly>
2021-09-24 09:59:06 -0700

    Add state & event listener

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
 /*----- constants -----*/
 
 
-/*----- app's state (variables) -----*/
 
+/*----- app's state (variables) -----*/
+let scores, results, winner;
 
 /*----- cached element references -----*/
 
 
 /*----- event listeners -----*/
+// call the function named handleClick
+// when the btn is clicked
+document.querySelector('button')
+  .addEventListener('click', handleClick);
 
 
 /*----- functions -----*/
+init();
 
+function init() {
+  scores = {
+    p: 0,
+    c: 0,
+    t: 0
+  };
+  // r = rock, p = paper & s = scissors
+  results = {
+    p: 'r',
+    c: 'r'
+  };
+  // p = player, c = computer, t = tie, 
+  winner = 't';
+  render();
+}
+
+// Transfer/visualize all state
+function render() {
+
+}
+
+function handleClick() {
+  console.log('clicked')
+}
 
--- readme.md
+++ readme.md
@@ -2,5 +2,5 @@
 
 To sync your code with the instructor's latest commit, run these two commands:
 
-- git fetch --all
-- git reset --hard origin/master
+git fetch --all
+git reset --hard origin/main
